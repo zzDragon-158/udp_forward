c0b24c4998e521b43c6c959c9de5f258521cafe8
zhangzilong <2211918137@qq.com>
2024-06-28 22:53:14 +0800

    zzDragon: 分别开发服务端和客户端，目前已支持客户端监听本地27015端口，发送给服务端，再由服务端转发到本地27015端口

--- client.cpp
+++ client.cpp
@@ -5,11 +5,11 @@
 #pragma comment(lib, "Ws2_32.lib")  // 链接Ws2_32.lib库文件
 
 using namespace std;
-const uint32_t udp_packet_size = 1 << (16 - 1);
+const uint32_t udp_packet_size = (1 << 16) - 1;
 struct DataPacket
 {
     sockaddr_in send_addr;
-    sockaddr_in recv_addr;
+    SOCKET recv_sock;
     char* data;
     uint32_t dataBytes;
     ~DataPacket() {
@@ -18,78 +18,199 @@ struct DataPacket
         }
     }
 };
-struct UdpThread
-{
-    sockaddr_in udpSocketAddr;
-    SOCKET udpSocket;
-    thread udpRecvThread;
-};
-vector<UdpThread> udpRecvFromRemotehostThreadPool;
-map<string, SOCKET> mapIp2Socket;
 using dataPacketPtr = shared_ptr<DataPacket>;
-queue<dataPacketPtr> packet_queue;
-mutex packetQueueMutex;
+struct UdpSocket
+{
+    sockaddr_in udpSockAddr;
+    SOCKET sock;
+};
+
+vector<UdpSocket> udpSocketsConnectWithRemoteHost;
+fd_set readSetRH;
+queue<dataPacketPtr> udpPacketFromRH;
+mutex mutexUdpPacketFromRH;
+vector<UdpSocket> udpSocketsConnectWithLocalHost;
+fd_set readSetLH;
+queue<dataPacketPtr> udpPacketFromLH;
+mutex mutexUdpPacketFromLH;
+
+map<string, SOCKET> mapIp2Socket;
+map<SOCKET, string> mapSocket2Ip;
+uint16_t client_port = 0;
+
 bool running_flag = true;
 
-void udpRecv(SOCKET sock, sockaddr_in sock_addr)
-{
-    // recv data
-    while (running_flag)
-    {
-        // init dataPacketPtr
-        dataPacketPtr dpp = make_shared<DataPacket>();
-        DataPacket* pdp = dpp.get();
-        // send_addr, dataBytes, recv_addr
-        char packet_buffer[udp_packet_size];
-        int addrLen = sizeof(pdp->send_addr);
-        pdp->dataBytes = recvfrom(
-            sock, packet_buffer, udp_packet_size, 0, 
-            reinterpret_cast<sockaddr*>(&pdp->send_addr), &addrLen
-        );
-        pdp->recv_addr = sock_addr;
-        // data
-        if (pdp->dataBytes == SOCKET_ERROR) {
-            cerr << __func__ <<": recv failed! error code " << WSAGetLastError() << endl;
-        } else {
-            pdp->data = new char[pdp->dataBytes];
-            memcpy(pdp->data, packet_buffer, pdp->dataBytes);
-            {
-                lock_guard<mutex> lock(packetQueueMutex);
-                packet_queue.push(dpp);
-            }
-        }
-    }
-    // free memory before exit this thread
-    // todo
-    closesocket(sock);
-}
-
-bool createListenSocket(sockaddr_in sock_addr, SOCKET& sock)
+SOCKET createListenSocket(sockaddr_in sockAddr)
 {
     // create udp socket
-    sock = socket(sock_addr.sin_family, SOCK_DGRAM, IPPROTO_UDP);
+    SOCKET sock = socket(sockAddr.sin_family, SOCK_DGRAM, IPPROTO_UDP);
     if (sock == INVALID_SOCKET)
     {
         cerr << __func__ << ": can't create socket! error code " << WSAGetLastError() << endl;
         running_flag = false;
-        return false;
+        return SOCKET_ERROR;
     }
     // bind udp socket
-    if (bind(sock, reinterpret_cast<sockaddr*>(&sock_addr), sizeof(sock_addr)) == SOCKET_ERROR) {
+    if (bind(sock, reinterpret_cast<sockaddr*>(&sockAddr), sizeof(sockAddr)) == SOCKET_ERROR) {
         cerr << __func__ << ": bind failed! error code " << WSAGetLastError() << endl;
         closesocket(sock);
         running_flag = false;
-        return false;
+        return SOCKET_ERROR;
     }
-    return true;
+    cout << "udp socket listenning from " << inet_ntoa(sockAddr.sin_addr)
+            << ":" << ntohs(sockAddr.sin_port) << endl;
+    return sock;
 }
 
-void udpSend()
+void udpRecvFromRemoteHost()
+{
+    // recv from remote host
+    for (uint16_t i = 3460; i < 3460 + 4; ++ i)
+    {
+        // init sockaddr_in
+        sockaddr_in sockAddr;
+        // init sockaddr_in
+        sockAddr.sin_family = AF_INET;
+        sockAddr.sin_addr.s_addr = INADDR_ANY;
+        sockAddr.sin_port = htons(i);
+        // init SOCKET and add to udpSocketsConnectWithRemoteHost
+        udpSocketsConnectWithRemoteHost.push_back({sockAddr, createListenSocket(sockAddr)});
+    }
+
+    // recv data
+    while (running_flag)
+    {
+        if (udpSocketsConnectWithRemoteHost.size() == 0)
+        {
+            this_thread::sleep_for(chrono::milliseconds(100));
+            continue;
+        }
+        FD_ZERO(&readSetRH);
+        for (int i = 0; i < udpSocketsConnectWithRemoteHost.size(); ++ i)
+        {
+            FD_SET(udpSocketsConnectWithRemoteHost[i].sock, &readSetRH);
+        }
+        if (select(0, &readSetRH, NULL, NULL, NULL) == SOCKET_ERROR) {
+            std::cerr << "Select failed with error: " << WSAGetLastError() << "\n";
+            break;
+        }
+        for (int i = 0; i < udpSocketsConnectWithRemoteHost.size(); ++ i)
+        {
+            if (FD_ISSET(udpSocketsConnectWithRemoteHost[i].sock, &readSetRH))
+            {
+                // init dataPacketPtr
+                dataPacketPtr dpp = make_shared<DataPacket>();
+                DataPacket* pdp = dpp.get();
+                // send_addr, dataBytes, recv_sock
+                char packet_buffer[udp_packet_size];
+                int addrLen = sizeof(pdp->send_addr);
+                pdp->dataBytes = recvfrom(
+                    udpSocketsConnectWithRemoteHost[i].sock, packet_buffer, udp_packet_size, 0, 
+                    reinterpret_cast<sockaddr*>(&pdp->send_addr), &addrLen
+                );
+                pdp->recv_sock = udpSocketsConnectWithRemoteHost[i].sock;
+                // data
+                if (pdp->dataBytes == SOCKET_ERROR)
+                {
+                    cerr << __func__ <<": recv failed! error code " << WSAGetLastError() << endl;
+                }
+                else
+                {
+                    pdp->data = new char[pdp->dataBytes];
+                    memcpy(pdp->data, packet_buffer, pdp->dataBytes);
+                    {
+                        lock_guard<mutex> lock(mutexUdpPacketFromRH);
+                        udpPacketFromRH.push(dpp);
+                    }
+                }
+            }
+        }
+    }
+
+    // close socket before exit current thread
+    for (int i = 0; i < udpSocketsConnectWithRemoteHost.size(); ++i)
+    {
+        closesocket(udpSocketsConnectWithRemoteHost[i].sock);
+    }
+}
+
+void udpRecvFromLocalHost()
+{
+    // recv from Local host
+    {
+        // init sockaddr_in
+        sockaddr_in sockAddr;
+        // init sockaddr_in
+        sockAddr.sin_family = AF_INET;
+        sockAddr.sin_addr.s_addr = INADDR_ANY;
+        sockAddr.sin_port = htons(27015);
+        // init SOCKET and add to udpSocketsConnectWithRemoteHost
+        udpSocketsConnectWithLocalHost.push_back({sockAddr, createListenSocket(sockAddr)});
+    }
+
+    // recv data
+    while (running_flag)
+    {
+        if (udpSocketsConnectWithLocalHost.size() == 0)
+        {
+            this_thread::sleep_for(chrono::milliseconds(100));
+            continue;
+        }
+        FD_ZERO(&readSetLH);
+        for (int i = 0; i < udpSocketsConnectWithLocalHost.size(); ++ i)
+        {
+            FD_SET(udpSocketsConnectWithLocalHost[i].sock, &readSetLH);
+        }
+        if (select(0, &readSetLH, NULL, NULL, NULL) == SOCKET_ERROR) {
+            std::cerr << "Select failed with error: " << WSAGetLastError() << "\n";
+            break;
+        }
+        for (int i = 0; i < udpSocketsConnectWithLocalHost.size(); ++ i)
+        {
+            if (FD_ISSET(udpSocketsConnectWithLocalHost[i].sock, &readSetLH))
+            {
+                // init dataPacketPtr
+                dataPacketPtr dpp = make_shared<DataPacket>();
+                DataPacket* pdp = dpp.get();
+                // send_addr, dataBytes, recv_sock
+                char packet_buffer[udp_packet_size];
+                int addrLen = sizeof(pdp->send_addr);
+                pdp->dataBytes = recvfrom(
+                    udpSocketsConnectWithLocalHost[i].sock, packet_buffer, udp_packet_size, 0, 
+                    reinterpret_cast<sockaddr*>(&pdp->send_addr), &addrLen
+                );
+                pdp->recv_sock = udpSocketsConnectWithLocalHost[i].sock;
+                // data
+                if (pdp->dataBytes == SOCKET_ERROR)
+                {
+                    cerr << __func__ <<": recv failed! error code " << WSAGetLastError() << endl;
+                }
+                else
+                {
+                    pdp->data = new char[pdp->dataBytes];
+                    memcpy(pdp->data, packet_buffer, pdp->dataBytes);
+                    {
+                        lock_guard<mutex> lock(mutexUdpPacketFromLH);
+                        udpPacketFromLH.push(dpp);
+                    }
+                }
+            }
+        }
+    }
+
+    // close socket before exit current thread
+    for (int i = 0; i < udpSocketsConnectWithLocalHost.size(); ++i)
+    {
+        closesocket(udpSocketsConnectWithLocalHost[i].sock);
+    }
+}
+
+void udpSendToLocalHost()               // one ip map one socket, no map then create one
 {
     // handle recved data packet
     while (running_flag)
     {
-        if (packet_queue.empty())
+        if (udpPacketFromRH.empty())
         {
             this_thread::sleep_for(chrono::milliseconds(100));
         }
@@ -97,14 +218,19 @@ void udpSend()
         {
             dataPacketPtr firstElement;
             {
-                lock_guard<mutex> lock(packetQueueMutex);
-                firstElement = packet_queue.front();
-                packet_queue.pop();
+                lock_guard<mutex> lock(mutexUdpPacketFromRH);
+                firstElement = udpPacketFromRH.front();
+                udpPacketFromRH.pop();
+            }
+            if (client_port == 0)
+            {
+                cerr << "I don't know who to send it to" << endl;
+                continue;
             }
             DataPacket* pdp = firstElement.get();
             sockaddr_in send_addr;
             send_addr.sin_family = AF_INET;
-            send_addr.sin_port = htons(27015);
+            send_addr.sin_port = htons(client_port);
             send_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
             SOCKET sendSocket;
             auto found = mapIp2Socket.find(inet_ntoa(pdp->send_addr.sin_addr));
@@ -116,8 +242,45 @@ void udpSend()
             {
                 sendSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
                 mapIp2Socket[inet_ntoa(pdp->send_addr.sin_addr)] = sendSocket;
+                mapSocket2Ip[sendSocket] = inet_ntoa(pdp->send_addr.sin_addr);
             }
             int sendResult = sendto(sendSocket, pdp->data, pdp->dataBytes, 0, 
+                reinterpret_cast<sockaddr*>(&send_addr), sizeof(send_addr));
+            if (sendResult == -1)
+            {
+                cerr << "sendto failed " << WSAGetLastError() << pdp->dataBytes << endl;
+            }
+            else
+            {
+                cout << "Sent " << sendResult << " bytes to " << inet_ntoa(send_addr.sin_addr) << ":" << ntohs(send_addr.sin_port) << endl;
+            }
+        }
+    }
+}
+
+void udpSendToRemoteHost()              // one socket map one ip, no map then continue, only for server endium
+{
+    // handle recved data packet
+    while (running_flag)
+    {
+        if (udpPacketFromLH.empty())
+        {
+            this_thread::sleep_for(chrono::milliseconds(100));
+        }
+        else
+        {
+            dataPacketPtr firstElement;
+            {
+                lock_guard<mutex> lock(mutexUdpPacketFromLH);
+                firstElement = udpPacketFromLH.front();
+                udpPacketFromLH.pop();
+            }
+            DataPacket* pdp = firstElement.get();
+            sockaddr_in send_addr;
+            send_addr.sin_family = AF_INET;
+            send_addr.sin_port = htons(8905);
+            send_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+            int sendResult = sendto(udpSocketsConnectWithRemoteHost[0].sock, pdp->data, pdp->dataBytes, 0, 
                 reinterpret_cast<sockaddr*>(&send_addr), sizeof(send_addr));
             if (sendResult == -1)
             {
@@ -141,38 +304,22 @@ int main() {
         return -1;
     }
 
-    // // recv from local host
-    // sockaddr_in sock_addr;
-    // sock_addr.sin_family = AF_INET;
-    // sock_addr.sin_port = htons(3460);
-    // sock_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    // SOCKET udpRecvFromLocalHostSocket;
-    // createListenSocket(sock_addr, udpRecvFromLocalHostSocket);
-    // cout << "udp socket listenning from " << inet_ntoa(sock_addr.sin_addr)
-    //             << ":" << ntohs(sock_addr.sin_port) << endl;
-    // thread udpRecvFromLocalHost(udpRecv, ref(udpRecvFromLocalHostSocket), sock_addr);
+    // init all thread
+    vector<thread> threadPool;
+    threadPool.push_back(thread(udpRecvFromRemoteHost));
+    threadPool.push_back(thread(udpRecvFromLocalHost));
+    threadPool.push_back(thread(udpSendToLocalHost));
+    threadPool.push_back(thread(udpSendToRemoteHost));
 
-    // recv from remote host
-    for (uint16_t i = 8905; i < 8905 + 4; ++ i)
+    // wait these thread exit
+    for_each(threadPool.begin(), threadPool.end(), [](thread& t)
     {
-        // init UdpThread
-        UdpThread uT;
-        // init sockaddr_in
-        uT.udpSocketAddr.sin_family = AF_INET;
-        uT.udpSocketAddr.sin_addr.s_addr = INADDR_ANY;
-        uT.udpSocketAddr.sin_port = htons(i);
-        // init SOCKET
-        createListenSocket(uT.udpSocketAddr, uT.udpSocket);
-        cout << "udp socket listenning from " << inet_ntoa(uT.udpSocketAddr.sin_addr)
-                << ":" << ntohs(uT.udpSocketAddr.sin_port) << endl;
-        // init thread
-        uT.udpRecvThread = move(thread(udpRecv, uT.udpSocket, uT.udpSocketAddr));
-        // add to udpRecvFromRemotehostThreadPool
-        udpRecvFromRemotehostThreadPool.push_back(move(uT));
-    }
+        if (t.joinable())
+        {
+            t.join();
+        }
+    });    
     
-
-    udpSend();
     WSACleanup();
 
     return 0;
--- server.cpp
+++ server.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 #include <thread>
-#include <WinSock2.h>
 #include <WS2tcpip.h>   // 包含Winsock2和网络相关的头文件
 
 #pragma comment(lib, "Ws2_32.lib")  // 链接Ws2_32.lib库文件
@@ -10,7 +9,7 @@ const uint32_t udp_packet_size = (1 << 16) - 1;
 struct DataPacket
 {
     sockaddr_in send_addr;
-    sockaddr_in recv_addr;
+    SOCKET recv_sock;
     char* data;
     uint32_t dataBytes;
     ~DataPacket() {
@@ -36,12 +35,14 @@ queue<dataPacketPtr> udpPacketFromLH;
 mutex mutexUdpPacketFromLH;
 
 map<string, SOCKET> mapIp2Socket;
+map<SOCKET, string> mapSocket2Ip;
+
 bool running_flag = true;
 
-SOCKET createListenSocket(sockaddr_in sock_addr)
+SOCKET createListenSocket(sockaddr_in sockAddr)
 {
     // create udp socket
-    SOCKET sock = socket(sock_addr.sin_family, SOCK_DGRAM, IPPROTO_UDP);
+    SOCKET sock = socket(sockAddr.sin_family, SOCK_DGRAM, IPPROTO_UDP);
     if (sock == INVALID_SOCKET)
     {
         cerr << __func__ << ": can't create socket! error code " << WSAGetLastError() << endl;
@@ -49,12 +50,14 @@ SOCKET createListenSocket(sockaddr_in sock_addr)
         return SOCKET_ERROR;
     }
     // bind udp socket
-    if (bind(sock, reinterpret_cast<sockaddr*>(&sock_addr), sizeof(sock_addr)) == SOCKET_ERROR) {
+    if (bind(sock, reinterpret_cast<sockaddr*>(&sockAddr), sizeof(sockAddr)) == SOCKET_ERROR) {
         cerr << __func__ << ": bind failed! error code " << WSAGetLastError() << endl;
         closesocket(sock);
         running_flag = false;
         return SOCKET_ERROR;
     }
+    cout << "udp socket listenning from " << inet_ntoa(sockAddr.sin_addr)
+            << ":" << ntohs(sockAddr.sin_port) << endl;
     return sock;
 }
 
@@ -71,13 +74,16 @@ void udpRecvFromRemoteHost()
         sockAddr.sin_port = htons(i);
         // init SOCKET and add to udpSocketsConnectWithRemoteHost
         udpSocketsConnectWithRemoteHost.push_back({sockAddr, createListenSocket(sockAddr)});
-        cout << "udp socket listenning from " << inet_ntoa(sockAddr.sin_addr)
-                << ":" << ntohs(sockAddr.sin_port) << endl;
     }
 
     // recv data
     while (running_flag)
     {
+        if (udpSocketsConnectWithRemoteHost.size() == 0)
+        {
+            this_thread::sleep_for(chrono::milliseconds(100));
+            continue;
+        }
         FD_ZERO(&readSetRH);
         for (int i = 0; i < udpSocketsConnectWithRemoteHost.size(); ++ i)
         {
@@ -94,14 +100,14 @@ void udpRecvFromRemoteHost()
                 // init dataPacketPtr
                 dataPacketPtr dpp = make_shared<DataPacket>();
                 DataPacket* pdp = dpp.get();
-                // send_addr, dataBytes, recv_addr
+                // send_addr, dataBytes, recv_sock
                 char packet_buffer[udp_packet_size];
                 int addrLen = sizeof(pdp->send_addr);
                 pdp->dataBytes = recvfrom(
                     udpSocketsConnectWithRemoteHost[i].sock, packet_buffer, udp_packet_size, 0, 
                     reinterpret_cast<sockaddr*>(&pdp->send_addr), &addrLen
                 );
-                pdp->recv_addr = udpSocketsConnectWithRemoteHost[i].udpSockAddr;
+                pdp->recv_sock = udpSocketsConnectWithRemoteHost[i].sock;
                 // data
                 if (pdp->dataBytes == SOCKET_ERROR)
                 {
@@ -119,8 +125,8 @@ void udpRecvFromRemoteHost()
             }
         }
     }
-    // free memory before exit this thread
-    // todo
+
+    // close socket before exit current thread
     for (int i = 0; i < udpSocketsConnectWithRemoteHost.size(); ++i)
     {
         closesocket(udpSocketsConnectWithRemoteHost[i].sock);
@@ -132,6 +138,11 @@ void udpRecvFromLocalHost()
     // recv data
     while (running_flag)
     {
+        if (udpSocketsConnectWithLocalHost.size() == 0)
+        {
+            this_thread::sleep_for(chrono::milliseconds(100));
+            continue;
+        }
         FD_ZERO(&readSetLH);
         for (int i = 0; i < udpSocketsConnectWithLocalHost.size(); ++ i)
         {
@@ -148,14 +159,14 @@ void udpRecvFromLocalHost()
                 // init dataPacketPtr
                 dataPacketPtr dpp = make_shared<DataPacket>();
                 DataPacket* pdp = dpp.get();
-                // send_addr, dataBytes, recv_addr
+                // send_addr, dataBytes, recv_sock
                 char packet_buffer[udp_packet_size];
                 int addrLen = sizeof(pdp->send_addr);
                 pdp->dataBytes = recvfrom(
                     udpSocketsConnectWithLocalHost[i].sock, packet_buffer, udp_packet_size, 0, 
                     reinterpret_cast<sockaddr*>(&pdp->send_addr), &addrLen
                 );
-                pdp->recv_addr = udpSocketsConnectWithLocalHost[i].udpSockAddr;
+                pdp->recv_sock = udpSocketsConnectWithLocalHost[i].sock;
                 // data
                 if (pdp->dataBytes == SOCKET_ERROR)
                 {
@@ -173,15 +184,15 @@ void udpRecvFromLocalHost()
             }
         }
     }
-    // free memory before exit this thread
-    // todo
-    for (int i = 0; i < udpSocketsConnectWithRemoteHost.size(); ++i)
+
+    // close socket before exit current thread
+    for (int i = 0; i < udpSocketsConnectWithLocalHost.size(); ++i)
     {
-        closesocket(udpSocketsConnectWithRemoteHost[i].sock);
+        closesocket(udpSocketsConnectWithLocalHost[i].sock);
     }
 }
 
-void udpSendToLocalHost()
+void udpSendToLocalHost()               // one ip map one socket, no map then create one
 {
     // handle recved data packet
     while (running_flag)
@@ -213,6 +224,53 @@ void udpSendToLocalHost()
             {
                 sendSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
                 mapIp2Socket[inet_ntoa(pdp->send_addr.sin_addr)] = sendSocket;
+                mapSocket2Ip[sendSocket] = inet_ntoa(pdp->send_addr.sin_addr);
+            }
+            int sendResult = sendto(sendSocket, pdp->data, pdp->dataBytes, 0, 
+                reinterpret_cast<sockaddr*>(&send_addr), sizeof(send_addr));
+            if (sendResult == -1)
+            {
+                cerr << "sendto failed " << WSAGetLastError() << pdp->dataBytes << endl;
+            }
+            else
+            {
+                cout << "Sent " << sendResult << " bytes to " << inet_ntoa(send_addr.sin_addr) << ":" << ntohs(send_addr.sin_port) << endl;
+            }
+        }
+    }
+}
+
+void udpSendToRemoteHost()              // one socket map one ip, no map then continue, only for server endium
+{
+    // handle recved data packet
+    while (running_flag)
+    {
+        if (udpPacketFromLH.empty())
+        {
+            this_thread::sleep_for(chrono::milliseconds(100));
+        }
+        else
+        {
+            dataPacketPtr firstElement;
+            {
+                lock_guard<mutex> lock(mutexUdpPacketFromLH);
+                firstElement = udpPacketFromLH.front();
+                udpPacketFromLH.pop();
+            }
+            DataPacket* pdp = firstElement.get();
+            sockaddr_in send_addr;
+            send_addr.sin_family = AF_INET;
+            send_addr.sin_port = htons(3460);
+            SOCKET sendSocket = udpSocketsConnectWithRemoteHost[0].sock;
+            auto found = mapSocket2Ip.find(sendSocket);
+            if (found != mapSocket2Ip.end())
+            {
+                send_addr.sin_addr.s_addr = inet_addr(found->second.c_str());
+            }
+            else
+            {
+                cerr << "I don't know who to send it to" << endl;
+                continue;
             }
             int sendResult = sendto(sendSocket, pdp->data, pdp->dataBytes, 0, 
                 reinterpret_cast<sockaddr*>(&send_addr), sizeof(send_addr));
@@ -238,21 +296,22 @@ int main() {
         return -1;
     }
 
-    // // recv from local host
-    // sockaddr_in sock_addr;
-    // sock_addr.sin_family = AF_INET;
-    // sock_addr.sin_port = htons(3460);
-    // sock_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    // SOCKET udpRecvFromLocalHostSocket;
-    // createListenSocket(sock_addr, udpRecvFromLocalHostSocket);
-    // cout << "udp socket listenning from " << inet_ntoa(sock_addr.sin_addr)
-    //             << ":" << ntohs(sock_addr.sin_port) << endl;
-    // thread udpRecvFromLocalHost(udpRecv, ref(udpRecvFromLocalHostSocket), sock_addr);
+    // init all thread
+    vector<thread> threadPool;
+    threadPool.push_back(thread(udpRecvFromRemoteHost));
+    threadPool.push_back(thread(udpRecvFromLocalHost));
+    threadPool.push_back(thread(udpSendToLocalHost));
+    threadPool.push_back(thread(udpSendToRemoteHost));
 
-
+    // wait these thread exit
+    for_each(threadPool.begin(), threadPool.end(), [](thread& t)
+    {
+        if (t.joinable())
+        {
+            t.join();
+        }
+    });    
     
-
-    udpSend();
     WSACleanup();
 
     return 0;
