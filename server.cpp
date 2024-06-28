254243a11fdded451cec3af0b4b8d86328620253
zzDragon <62492894+zzDragon-158@users.noreply.github.com>
2024-06-28 18:39:00 +0800

    zzDragon: 没改完，先同步到云端

--- server.cpp
+++ server.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 #include <thread>
+#include <WinSock2.h>
 #include <WS2tcpip.h>   // 包含Winsock2和网络相关的头文件
 
 #pragma comment(lib, "Ws2_32.lib")  // 链接Ws2_32.lib库文件
 
 using namespace std;
-const uint32_t udp_packet_size = 1 << (16 - 1);
+const uint32_t udp_packet_size = (1 << 16) - 1;
 struct DataPacket
 {
     sockaddr_in send_addr;
@@ -18,78 +19,174 @@ struct DataPacket
         }
     }
 };
-struct UdpThread
-{
-    sockaddr_in udpSocketAddr;
-    SOCKET udpSocket;
-    thread udpRecvThread;
-};
-vector<UdpThread> udpRecvFromRemotehostThreadPool;
-map<string, SOCKET> mapIp2Socket;
 using dataPacketPtr = shared_ptr<DataPacket>;
-queue<dataPacketPtr> packet_queue;
-mutex packetQueueMutex;
+struct UdpSocket
+{
+    sockaddr_in udpSockAddr;
+    SOCKET sock;
+};
+
+vector<UdpSocket> udpSocketsConnectWithRemoteHost;
+fd_set readSetRH;
+queue<dataPacketPtr> udpPacketFromRH;
+mutex mutexUdpPacketFromRH;
+vector<UdpSocket> udpSocketsConnectWithLocalHost;
+fd_set readSetLH;
+queue<dataPacketPtr> udpPacketFromLH;
+mutex mutexUdpPacketFromLH;
+
+map<string, SOCKET> mapIp2Socket;
 bool running_flag = true;
 
-void udpRecv(SOCKET sock, sockaddr_in sock_addr)
-{
-    // recv data
-    while (running_flag)
-    {
-        // init dataPacketPtr
-        dataPacketPtr dpp = make_shared<DataPacket>();
-        DataPacket* pdp = dpp.get();
-        // send_addr, dataBytes, recv_addr
-        char packet_buffer[udp_packet_size];
-        int addrLen = sizeof(pdp->send_addr);
-        pdp->dataBytes = recvfrom(
-            sock, packet_buffer, udp_packet_size, 0, 
-            reinterpret_cast<sockaddr*>(&pdp->send_addr), &addrLen
-        );
-        pdp->recv_addr = sock_addr;
-        // data
-        if (pdp->dataBytes == SOCKET_ERROR) {
-            cerr << __func__ <<": recv failed! error code " << WSAGetLastError() << endl;
-        } else {
-            pdp->data = new char[pdp->dataBytes];
-            memcpy(pdp->data, packet_buffer, pdp->dataBytes);
-            {
-                lock_guard<mutex> lock(packetQueueMutex);
-                packet_queue.push(dpp);
-            }
-        }
-    }
-    // free memory before exit this thread
-    // todo
-    closesocket(sock);
-}
-
-bool createListenSocket(sockaddr_in sock_addr, SOCKET& sock)
+SOCKET createListenSocket(sockaddr_in sock_addr)
 {
     // create udp socket
-    sock = socket(sock_addr.sin_family, SOCK_DGRAM, IPPROTO_UDP);
+    SOCKET sock = socket(sock_addr.sin_family, SOCK_DGRAM, IPPROTO_UDP);
     if (sock == INVALID_SOCKET)
     {
         cerr << __func__ << ": can't create socket! error code " << WSAGetLastError() << endl;
         running_flag = false;
-        return false;
+        return SOCKET_ERROR;
     }
     // bind udp socket
     if (bind(sock, reinterpret_cast<sockaddr*>(&sock_addr), sizeof(sock_addr)) == SOCKET_ERROR) {
         cerr << __func__ << ": bind failed! error code " << WSAGetLastError() << endl;
         closesocket(sock);
         running_flag = false;
-        return false;
+        return SOCKET_ERROR;
     }
-    return true;
+    return sock;
 }
 
-void udpSend()
+void udpRecvFromRemoteHost()
+{
+    // recv from remote host
+    for (uint16_t i = 8905; i < 8905 + 4; ++ i)
+    {
+        // init sockaddr_in
+        sockaddr_in sockAddr;
+        // init sockaddr_in
+        sockAddr.sin_family = AF_INET;
+        sockAddr.sin_addr.s_addr = INADDR_ANY;
+        sockAddr.sin_port = htons(i);
+        // init SOCKET and add to udpSocketsConnectWithRemoteHost
+        udpSocketsConnectWithRemoteHost.push_back({sockAddr, createListenSocket(sockAddr)});
+        cout << "udp socket listenning from " << inet_ntoa(sockAddr.sin_addr)
+                << ":" << ntohs(sockAddr.sin_port) << endl;
+    }
+
+    // recv data
+    while (running_flag)
+    {
+        FD_ZERO(&readSetRH);
+        for (int i = 0; i < udpSocketsConnectWithRemoteHost.size(); ++ i)
+        {
+            FD_SET(udpSocketsConnectWithRemoteHost[i].sock, &readSetRH);
+        }
+        if (select(0, &readSetRH, NULL, NULL, NULL) == SOCKET_ERROR) {
+            std::cerr << "Select failed with error: " << WSAGetLastError() << "\n";
+            break;
+        }
+        for (int i = 0; i < udpSocketsConnectWithRemoteHost.size(); ++ i)
+        {
+            if (FD_ISSET(udpSocketsConnectWithRemoteHost[i].sock, &readSetRH))
+            {
+                // init dataPacketPtr
+                dataPacketPtr dpp = make_shared<DataPacket>();
+                DataPacket* pdp = dpp.get();
+                // send_addr, dataBytes, recv_addr
+                char packet_buffer[udp_packet_size];
+                int addrLen = sizeof(pdp->send_addr);
+                pdp->dataBytes = recvfrom(
+                    udpSocketsConnectWithRemoteHost[i].sock, packet_buffer, udp_packet_size, 0, 
+                    reinterpret_cast<sockaddr*>(&pdp->send_addr), &addrLen
+                );
+                pdp->recv_addr = udpSocketsConnectWithRemoteHost[i].udpSockAddr;
+                // data
+                if (pdp->dataBytes == SOCKET_ERROR)
+                {
+                    cerr << __func__ <<": recv failed! error code " << WSAGetLastError() << endl;
+                }
+                else
+                {
+                    pdp->data = new char[pdp->dataBytes];
+                    memcpy(pdp->data, packet_buffer, pdp->dataBytes);
+                    {
+                        lock_guard<mutex> lock(mutexUdpPacketFromRH);
+                        udpPacketFromRH.push(dpp);
+                    }
+                }
+            }
+        }
+    }
+    // free memory before exit this thread
+    // todo
+    for (int i = 0; i < udpSocketsConnectWithRemoteHost.size(); ++i)
+    {
+        closesocket(udpSocketsConnectWithRemoteHost[i].sock);
+    }
+}
+
+void udpRecvFromLocalHost()
+{
+    // recv data
+    while (running_flag)
+    {
+        FD_ZERO(&readSetLH);
+        for (int i = 0; i < udpSocketsConnectWithLocalHost.size(); ++ i)
+        {
+            FD_SET(udpSocketsConnectWithLocalHost[i].sock, &readSetLH);
+        }
+        if (select(0, &readSetLH, NULL, NULL, NULL) == SOCKET_ERROR) {
+            std::cerr << "Select failed with error: " << WSAGetLastError() << "\n";
+            break;
+        }
+        for (int i = 0; i < udpSocketsConnectWithLocalHost.size(); ++ i)
+        {
+            if (FD_ISSET(udpSocketsConnectWithLocalHost[i].sock, &readSetLH))
+            {
+                // init dataPacketPtr
+                dataPacketPtr dpp = make_shared<DataPacket>();
+                DataPacket* pdp = dpp.get();
+                // send_addr, dataBytes, recv_addr
+                char packet_buffer[udp_packet_size];
+                int addrLen = sizeof(pdp->send_addr);
+                pdp->dataBytes = recvfrom(
+                    udpSocketsConnectWithLocalHost[i].sock, packet_buffer, udp_packet_size, 0, 
+                    reinterpret_cast<sockaddr*>(&pdp->send_addr), &addrLen
+                );
+                pdp->recv_addr = udpSocketsConnectWithLocalHost[i].udpSockAddr;
+                // data
+                if (pdp->dataBytes == SOCKET_ERROR)
+                {
+                    cerr << __func__ <<": recv failed! error code " << WSAGetLastError() << endl;
+                }
+                else
+                {
+                    pdp->data = new char[pdp->dataBytes];
+                    memcpy(pdp->data, packet_buffer, pdp->dataBytes);
+                    {
+                        lock_guard<mutex> lock(mutexUdpPacketFromLH);
+                        udpPacketFromLH.push(dpp);
+                    }
+                }
+            }
+        }
+    }
+    // free memory before exit this thread
+    // todo
+    for (int i = 0; i < udpSocketsConnectWithRemoteHost.size(); ++i)
+    {
+        closesocket(udpSocketsConnectWithRemoteHost[i].sock);
+    }
+}
+
+void udpSendToLocalHost()
 {
     // handle recved data packet
     while (running_flag)
     {
-        if (packet_queue.empty())
+        if (udpPacketFromRH.empty())
         {
             this_thread::sleep_for(chrono::milliseconds(100));
         }
@@ -97,9 +194,9 @@ void udpSend()
         {
             dataPacketPtr firstElement;
             {
-                lock_guard<mutex> lock(packetQueueMutex);
-                firstElement = packet_queue.front();
-                packet_queue.pop();
+                lock_guard<mutex> lock(mutexUdpPacketFromRH);
+                firstElement = udpPacketFromRH.front();
+                udpPacketFromRH.pop();
             }
             DataPacket* pdp = firstElement.get();
             sockaddr_in send_addr;
@@ -152,24 +249,7 @@ int main() {
     //             << ":" << ntohs(sock_addr.sin_port) << endl;
     // thread udpRecvFromLocalHost(udpRecv, ref(udpRecvFromLocalHostSocket), sock_addr);
 
-    // recv from remote host
-    for (uint16_t i = 8905; i < 8905 + 4; ++ i)
-    {
-        // init UdpThread
-        UdpThread uT;
-        // init sockaddr_in
-        uT.udpSocketAddr.sin_family = AF_INET;
-        uT.udpSocketAddr.sin_addr.s_addr = INADDR_ANY;
-        uT.udpSocketAddr.sin_port = htons(i);
-        // init SOCKET
-        createListenSocket(uT.udpSocketAddr, uT.udpSocket);
-        cout << "udp socket listenning from " << inet_ntoa(uT.udpSocketAddr.sin_addr)
-                << ":" << ntohs(uT.udpSocketAddr.sin_port) << endl;
-        // init thread
-        uT.udpRecvThread = move(thread(udpRecv, uT.udpSocket, uT.udpSocketAddr));
-        // add to udpRecvFromRemotehostThreadPool
-        udpRecvFromRemotehostThreadPool.push_back(move(uT));
-    }
+
     
 
     udpSend();
--- udp_send.py
+++ udp_send.py
@@ -2,22 +2,24 @@ import socket
 
 def main():
     # 目标地址和端口
-    target_host = '172.16.100.204'
-    target_port = 8906
+    target_host = '127.0.0.1'
+    target_port = 5001
 
     # 创建UDP套接字
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # 绑定本地端口（可选）
-    # client_socket.bind(('127.0.0.1', 3461))
+    # client_socket.bind(('127.0.0.1', 5000))
 
     # 要发送的数据
-    message = "Hello, UDP 172!"
+    message = "Hello, UDP 5001!"
 
     try:
         # 发送数据到指定地址和端口
         client_socket.sendto(message.encode(), (target_host, target_port))
-        print(f"Sent message to {target_host}:{target_port}")
+        print(f"Sent message to {target_host}:{5001}")
+        client_socket.sendto(message.encode(), (target_host, target_port))
+        print(f"Sent message to {target_host}:{5002}")
 
     except Exception as e:
         print(f"Error while sending data: {e}")
